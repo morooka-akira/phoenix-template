468837aca5f50c26982cfdfca8db40a49437abdb
Heiya <morooka.akira.inon29@gmail.com>
2020-05-23 16:04:07 +0900

    udate.

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -16,8 +16,9 @@ defmodule HelloWeb.Router do
   scope "/", HelloWeb do
     pipe_through :browser
 
-    get "/hello", HelloController, :index
     get "/", PageController, :index
+    get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
   end
 
   # Other scopes may use custom stacks.
